2aae68f8d800ce13db72b3eaab5291e2c044f0f9
Isaac Priestley <isaac.priestley@babylist.com>
2024-02-17 13:25:36 -0500

    Authentication is working

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import './App.css'
 import Home from './components/Home'
 import Profile from './components/Profile'
 import Navbar from './components/Navbar'
+import Login from './components/Login';
 import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthProvider>
     </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,21 @@
+import { useAuth } from "../contexts/AuthContext";
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+  const { user } = useAuth()
+
   return (
     <nav>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
+        { user && (<>
+          <li><Link to="/profile">Profile</Link></li>
+        </>) }
+        { !user && (
+          <li><Link to="/login">Login</Link></li>
+        ) }
       </ul>
     </nav>
   )
