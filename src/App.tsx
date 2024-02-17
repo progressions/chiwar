9f68757e07605d52e465bd4880399d50c5a8ed16
Isaac Priestley <isaac.priestley@babylist.com>
2024-02-17 12:54:36 -0500

    Working

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import React from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
-import Fights from './components/Fights'
-import Campaigns from './components/Campaigns'
-import Profile from './components/Profile'
-import Login from './components/Login'
-import Navbar from './components/Navbar'
-import RequireAuth from './components/RequireAuth'
-import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   return (
-    <AuthProvider>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/fights" element={<RequireAuth><Fights /></RequireAuth>} />
-          <Route path="/fights/:id" element={<RequireAuth><Fights /></RequireAuth>} />
-          <Route path="/campaigns" element={<RequireAuth><Campaigns /></RequireAuth>} />
-          <Route path="/campaigns/:id" element={<RequireAuth><Campaigns /></RequireAuth>} />
-          <Route path="/profile" element={<RequireAuth><Profile /></RequireAuth>} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </AuthProvider>
+    <Home />
   )
 }
 
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,86 @@
+import { useMemo, useCallback, useState, useEffect, createContext, useContext } from 'react';
+import axios from "axios";
+import { jwtDecode, JwtPayload } from "jwt-decode";
+import Cookies from "universal-cookie";
+import type { User } from "@/types/types";
+
+export type AuthContextType = {
+  user: User | null
+  login: (email: string, password: string) => Promise<void>
+  logout: () => void
+  jwt: JwtPayload | null
+  validateToken: () => string | false
+}
+
+export type AuthProviderProps = {
+  children: React.ReactNode
+}
+
+const AuthContext = createContext<AuthContextType>({ user: null, login: async (email: string, password: string) => {}, logout: () => {}, jwt: null, validateToken: () => false })
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const cookies = useMemo(() => new Cookies(), [])
+  const [user, setUser] = useState<User | null>(null)
+  const [jwt, setJwt] = useState<JwtPayload | null>(null)
+
+  const validateToken = useCallback(() => {
+    const token = cookies.get("jwt_authorization")
+    if (token) {
+      const decoded = jwtDecode(token) as JwtPayload;
+      if ((decoded.exp || 0) * 1000 < Date.now()) {
+        cookies.remove("jwt_authorization");
+        return false; // Explicitly returning false here
+      } else {
+        setUser(decoded as User);
+        setJwt(token);
+        return token; // Returning the token if valid
+      }
+    }
+    return false; // Make sure to return false or null explicitly if token is not present or valid
+  }, [cookies])
+
+  useEffect(() => {
+    validateToken()
+  }, [validateToken])
+
+  const login = async (email: string, password: string): Promise<void> => {
+    const response = await axios({
+      method: 'post',
+      url: 'http://localhost:3000/users/sign_in',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Headers': "true",
+      },
+      data: JSON.stringify({user: { email, password }})
+    }).catch((error) => {
+      return error
+    })
+
+    if (response.status === 200) {
+      const authToken = response.headers.get('authorization')
+      const decoded = jwtDecode(authToken)
+      if (!decoded) return
+      cookies.set("jwt_authorization", authToken, {
+        expires: new Date((decoded.exp || 0) * 1000),
+      })
+      setUser(decoded as User)
+    }
+
+    return response
+  }
+
+  const logout = (): void => {
+    setUser(null)
+    cookies.remove("jwt_authorization")
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, jwt, validateToken }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => {
+  return useContext(AuthContext)
+}
